46a83ba0ab4667439bd249c5dc7b569c139dc780
Zaiming Shi <zmstone@gmail.com>
2016-05-24 18:42:25 +0200

    add decoder hook

--- src/avro_binary_decoder.erl
+++ src/avro_binary_decoder.erl
@@ -34,11 +34,51 @@
         , decode_stream/4
         ]).
 
+-export([ pretty_print_debug_hook/0
+        ]).
+
 -include("erlavro.hrl").
 
--define(DEFAULT_HOOK, fun(__Type, __Data) -> ok end).
+%% Decoding hook is a function to be evaluated when decoding:
+%% 1. primitives
+%% 2. each field/element of complex types.
+%%
+%% A hook fun can be used to fast skipping undesired data fields of records
+%% or undesired data of big maps etc.
+%% For example, to dig out only the field named "MyField" in "MyRecord", the
+%% hook may probably look like:
+%%
+%% fun(Type, SubNameOrIndex, Data, DecodeFun) ->
+%%      case {avro:get_type_fullname(Type), SubNameOrIndex} of
+%%        {"MyRecord.example.com", "MyField"} ->
+%%          DecodeFun(Data);
+%%        {"MyRecord.example.com", _OtherFields} ->
+%%          ignored;
+%%        _OtherType ->
+%%          DecodeFun(Data)
+%%      end
+%% end.
+%%
+%% A hook fun can be used for debug. For example, blow hook should print
+%% the decoding stack along the decode function traverses through the bytes.
+%%
+%% fun(Type, SubNameOrIndex, Data, DecodeFun) ->
+%%      SubInfo = case is_integer(SubNameOrIndex) of
+%%                  true  -> integer_to_list(SubNameOrIndex);
+%%                  false -> SubNameOrIndex
+%%                end,
+%%      io:format("~s.~s\n", [avro:get_type_name(Type), SubInfo]),
+%%      DecodeFun(Data)
+%% end
+%%
+%% A hook can also be used as a dirty patch to fix some corrupted data.
+%%
+-type hook_fun() :: fun((avro_type(), string() | integer(), binary(),
+                        fun((binary()) -> term())) -> term()).
+%% By default, the hook fun does nothing else but calling the decode function.
+-define(DEFAULT_HOOK,
+        fun(__Type__, __SubNameOrId__, Data, DecodeFun) -> DecodeFun(Data) end).
 
--type hook_fun() :: fun((avro_type(), binary()) -> ok).
 -type lkup_fun() :: fun((string()) -> avro_type()).
 
 %%%_* APIs =====================================================================
@@ -53,71 +93,121 @@ decode(IoData, Type, Lkup) when is_function(Lkup, 1) ->
 %% @end
 -spec decode(iodata(), string() | avro_type(),
              lkup_fun(), hook_fun()) -> term().
-decode(IoData, Type, Lkup, _Hook) ->
+decode(IoData, Type, Lkup, Hook) when is_function(Lkup, 1) ->
   %% return decoded value as raw erlang term directly
-  {Value, <<>>} = do_decode(IoData, Type, Lkup),
+  {Value, <<>>} = do_decode(IoData, Type, Lkup, Hook),
   Value.
 
 %% @doc decode_stream/4 equivalent with default hook fun.
 -spec decode_stream(iodata(), string() | avro_type(),
                     fun((string()) -> avro_type())) -> term().
-decode_stream(IoData, Type, LookupFun) ->
-  decode_stream(IoData, Type, LookupFun, ?DEFAULT_HOOK).
+decode_stream(IoData, Type, Lkup) when is_function(Lkup, 1) ->
+  decode_stream(IoData, Type, Lkup, ?DEFAULT_HOOK).
 
 %% @doc Decode the header of a byte stream, return unwrapped value and tail
 %% bytes in a tuple.
 %% @end
-decode_stream(IoData, Type, LookupFun, _Hook) ->
-  do_decode(IoData, Type, LookupFun).
+decode_stream(IoData, Type, Lkup, Hook) when is_function(Lkup, 1) ->
+  do_decode(IoData, Type, Lkup, Hook).
+
+%% @doc Return a function to be used as the decode hook.
+%% The hook prints the type tree with indentation, and the leaf values.
+%% @end
+-spec pretty_print_debug_hook() -> hook_fun().
+pretty_print_debug_hook() ->
+  fun(T, SubInfo, Data, DecodeFun) ->
+    Name = avro:get_type_fullname(T),
+    Indentation =
+      case get(avro_binary_decoder_pp_indentation) of
+        undefined -> 0;
+        Indentati -> Indentati
+      end,
+    IndentationStr = lists:duplicate(Indentation * 2, $\s),
+    ToPrint =
+      [ IndentationStr
+      , Name
+      , case SubInfo of
+          []                   -> ": ";
+          I when is_integer(I) -> [$., integer_to_list(I), "\n"];
+          S                    -> [$., S, "\n"]
+        end
+      ],
+    io:format("~s", [ToPrint]),
+    _ = put(avro_binary_decoder_pp_indentation, Indentation + 1),
+    {Result, Tail} = DecodeFun(Data),
+    %% print empty array and empty map
+    case SubInfo =/= [] andalso Result =:= [] of
+      true  -> io:format("~s  []\n", [IndentationStr]);
+      false -> ok
+    end,
+    %% print the value if it's a leaf in the type tree
+    _ = SubInfo =:= [] andalso io:format("~1000000p\n", [Result]),
+    _ = put(avro_binary_decoder_pp_indentation, Indentation),
+    {Result, Tail}
+  end.
 
 %%%_* Internal functions =======================================================
 
-do_decode(IoList, Type, LookupFun) when is_list(IoList) ->
-  do_decode(iolist_to_binary(IoList), Type, LookupFun);
-do_decode(Bin, TypeName, LookupFun) when is_list(TypeName) ->
-  Type = LookupFun(TypeName),
-  do_decode(Bin, Type, LookupFun);
-do_decode(Bin, Type, LookupFun) ->
-  dec(Bin, Type, LookupFun).
+do_decode(IoList, Type, Lkup, Hook) when is_list(IoList) ->
+  do_decode(iolist_to_binary(IoList), Type, Lkup, Hook);
+do_decode(Bin, TypeName, Lkup, Hook) when is_list(TypeName) ->
+  do_decode(Bin, Lkup(TypeName), Lkup, Hook);
+do_decode(Bin, Type, Lkup, Hook) when is_function(Hook, 4) ->
+  dec(Bin, Type, Lkup, Hook).
 
-dec(Bin, T, _Lkup) when ?AVRO_IS_PRIMITIVE_TYPE(T) ->
-  prim(Bin, T#avro_primitive_type.name);
-dec(Bin, T, Lkup) when ?AVRO_IS_RECORD_TYPE(T) ->
+dec(Bin, T, _Lkup, Hook) when ?AVRO_IS_PRIMITIVE_TYPE(T) ->
+  Hook(T, "", Bin, fun(B) -> prim(B, T#avro_primitive_type.name) end);
+dec(Bin, T, Lkup, Hook) when ?AVRO_IS_RECORD_TYPE(T) ->
   FieldTypes = avro_record:get_all_field_types(T),
   {FieldValuesReversed, Tail} =
     lists:foldl(
       fun({FieldName, FieldTypeOrName}, {Values, BinIn}) ->
-        {Value, BinOut} = do_decode(BinIn, FieldTypeOrName, Lkup),
+        {Value, BinOut} =
+          Hook(T, FieldName, BinIn,
+               fun(B) -> do_decode(B, FieldTypeOrName, Lkup, Hook) end),
         {[{FieldName, Value} | Values], BinOut}
       end, {[], Bin}, FieldTypes),
   FieldValues = lists:reverse(FieldValuesReversed),
   Name = avro:get_type_fullname(T),
   {{Name, FieldValues}, Tail};
-dec(Bin, T, _Lkup) when ?AVRO_IS_ENUM_TYPE(T) ->
+dec(Bin, T, _Lkup, Hook) when ?AVRO_IS_ENUM_TYPE(T) ->
   {Index, Tail} = int(Bin),
-  Symbol = avro_enum:get_symbol_from_index(T, Index),
-  {Symbol, Tail};
-dec(Bin, T, Lkup) when ?AVRO_IS_ARRAY_TYPE(T) ->
+  Hook(T, Index, Tail,
+       fun(B) ->
+         Symbol = avro_enum:get_symbol_from_index(T, Index),
+         {Symbol, B}
+       end);
+dec(Bin, T, Lkup, Hook) when ?AVRO_IS_ARRAY_TYPE(T) ->
   ItemsType = avro_array:get_items_type(T),
-  ItemDecodeFun = fun(BinIn) -> do_decode(BinIn, ItemsType, Lkup) end,
-  block(Bin, ItemDecodeFun);
-dec(Bin, T, Lkup) when ?AVRO_IS_MAP_TYPE(T) ->
+  ItemDecodeFun =
+    fun(Index, BinIn) ->
+      Hook(T, Index, BinIn,
+           fun(B) -> do_decode(B, ItemsType, Lkup, Hook) end)
+    end,
+  blocks(Bin, ItemDecodeFun);
+dec(Bin, T, Lkup, Hook) when ?AVRO_IS_MAP_TYPE(T) ->
   ItemsType = avro_map:get_items_type(T),
   ItemDecodeFun =
-    fun(BinIn) ->
+    fun(_Index, BinIn) ->
       {Key, Tail1} = prim(BinIn, "string"),
-      {Value, Tail} = do_decode(Tail1, ItemsType, Lkup),
+      {Value, Tail} =
+        Hook(T, Key, Tail1,
+             fun(B) -> do_decode(B, ItemsType, Lkup, Hook) end),
       {{Key, Value}, Tail}
     end,
-  block(Bin, ItemDecodeFun);
-dec(Bin, T, Lkup) when ?AVRO_IS_UNION_TYPE(T) ->
+  blocks(Bin, ItemDecodeFun);
+dec(Bin, T, Lkup, Hook) when ?AVRO_IS_UNION_TYPE(T) ->
   {Index, Tail} = long(Bin),
   {ok, ChildType} = avro_union:lookup_child_type(T, Index),
-  do_decode(Tail, ChildType, Lkup);
-dec(Bin, T, _Lkup) when ?AVRO_IS_FIXED_TYPE(T) ->
-  Size = avro_fixed:get_size(T),
-  <<Value:Size/binary, Tail/binary>> = Bin,
-  {Value, Tail}.
+  Hook(T, Index, Tail,
+       fun(B) -> do_decode(B, ChildType, Lkup, Hook) end);
+dec(Bin, T, _Lkup, Hook) when ?AVRO_IS_FIXED_TYPE(T) ->
+  Hook(T, "", Bin,
+       fun(B) ->
+          Size = avro_fixed:get_size(T),
+          <<Value:Size/binary, Tail/binary>> = B,
+          {Value, Tail}
+       end).
 
 %% @private Decode primitive values.
 %% NOTE: keep all binary decoding exceptions to error:{badmatch, _}
@@ -149,9 +239,10 @@ bytes(Bin) ->
   <<Bytes:Size/binary, Tail/binary>> = Rest,
   {Bytes, Tail}.
 
-block(Bin, ItemDecodeFun) -> block(Bin, ItemDecodeFun, _Acc = []).
+blocks(Bin, ItemDecodeFun) ->
+  blocks(Bin, ItemDecodeFun, _Index = 1, _Acc = []).
 
-block(Bin, ItemDecodeFun, Acc) ->
+blocks(Bin, ItemDecodeFun, Index, Acc) ->
   {Count0, Rest} = long(Bin),
   case Count0 =:= 0 of
     true ->
@@ -169,14 +260,14 @@ block(Bin, ItemDecodeFun, Acc) ->
           false ->
             {Count0, Rest}
         end,
-      block(Tail0, ItemDecodeFun, Acc, Count)
+      block(Tail0, ItemDecodeFun, Index, Acc, Count)
   end.
 
-block(Bin, ItemDecodeFun, Acc, 0) ->
-  block(Bin, ItemDecodeFun, Acc);
-block(Bin, ItemDecodeFun, Acc, Count) ->
-  {Item, Tail} = ItemDecodeFun(Bin),
-  block(Tail, ItemDecodeFun, [Item | Acc], Count-1).
+block(Bin, ItemDecodeFun, Index, Acc, 0) ->
+  blocks(Bin, ItemDecodeFun, Index, Acc);
+block(Bin, ItemDecodeFun, Index, Acc, Count) ->
+  {Item, Tail} = ItemDecodeFun(Index, Bin),
+  block(Tail, ItemDecodeFun, Index+1, [Item | Acc], Count-1).
 
 int(Bin) -> zigzag(varint(Bin, 0, 0, 32)).
 long(Bin) -> zigzag(varint(Bin, 0, 0, 64)).
@@ -192,6 +283,7 @@ varint(Bin, Acc, AccBits, MaxBits) ->
     true  -> {NewAcc, Tail};
     false -> varint(Tail, NewAcc, AccBits + 7, MaxBits)
   end.
+
 
 %%%_* Tests ====================================================================
 
@@ -234,8 +326,7 @@ decode_float_test() ->
   Typed = avro_primitive:float(V_orig),
   Bin = avro_binary_encoder:encode_value(Typed),
   V_dec = decode_t(Bin, Type),
-  abs(V_orig - V_dec) < 0.000001 orelse
-    erlang:error({V_orig, V_dec}).
+  ?assert(abs(V_orig - V_dec) < 0.000001).
 
 decode_double_test() ->
   Type = avro_primitive:double_type(),
@@ -243,8 +334,7 @@ decode_double_test() ->
   Typed = avro_primitive:double(V_orig),
   Bin = avro_binary_encoder:encode_value(Typed),
   V_dec = decode_t(Bin, Type),
-  abs(V_orig - V_dec) < 0.000000000001 orelse
-    erlang:error({V_orig, V_dec}).
+  ?assert(abs(V_orig - V_dec) < 0.000000000001).
 
 decode_empty_bytes_test() ->
   ?assertEqual(<<>>, decode_t(<<0>>, avro_primitive:bytes_type())).
@@ -323,6 +413,12 @@ sample_record_type() ->
     , avro_record:define_field("string", avro_primitive:string_type(),
 			       [ {doc, "string f"}
 			       ])
+    , avro_record:define_field("array", avro_array:type(avro_primitive:long_type()),
+			       [ {doc, "array f"}
+			       ])
+    , avro_record:define_field("map", avro_map:type(avro_primitive:long_type()),
+			       [ {doc, "map f"}
+			       ])
     ],
     [ {namespace, "com.klarna.test.bix"}
     , {doc, "Record documentation"}]).
@@ -334,7 +430,9 @@ sample_record_binary() ->
 	 <<84, 248, 45, 64>>, % float
    <<244, 214, 67, 84, 251, 33, 9, 64>>, % double
 	 [22, <<98, 121, 116, 101, 115, 32, 118, 97, 108, 117, 101>>], % bytes
-	 [12, <<115, 116, 114, 105, 110, 103>>] % string
+	 [12, <<115, 116, 114, 105, 110, 103>>], % string
+   [2, 0, 0], %% array
+   [0] %% map
   ].
 
 decode_record_test() ->
@@ -347,7 +445,27 @@ decode_record_test() ->
                , {"double", _}
                , {"bytes",  <<"bytes value">>}
                , {"string", "string"}
+               , {"array",  [0]}
+               , {"map",    []}
                ], Fields).
+
+decode_with_hook_test() ->
+  Binary = sample_record_binary(),
+  Schema = sample_record_type(),
+  Lkup = fun(_) -> exit(error) end,
+  {Name, Fields} = decode(Binary, Schema, Lkup, pretty_print_debug_hook()),
+  ?assertEqual(Name, "com.klarna.test.bix.SampleRecord"),
+  ?assertMatch([ {"bool",   true}
+               , {"int",    100}
+               , {"long",   123456789123456789}
+               , {"float",  _}
+               , {"double", _}
+               , {"bytes",  <<"bytes value">>}
+               , {"string", "string"}
+               , {"array",  [0]}
+               , {"map",    []}
+               ], Fields).
+
 
 -endif.
 
