d1d6c104bd0f53a0b90b3defeafbecf68e3441f9
Randy Secrist <randy.secrist@gmail.com>
2017-11-21 03:47:11 -0700

    allow default values to be null (#51)
    Allow record field default value to be 'null'

--- src/avro_json_decoder.erl
+++ src/avro_json_decoder.erl
@@ -214,10 +214,14 @@ parse_record_field(Attrs, Lkup) ->
   }.
 
 %% @private
--spec parse_default_value(undefined | json_value(), avro_type(), lkup_fun()) ->
-        undefined | avro_value().
+-spec parse_default_value(null | undefined | json_value(), avro_type(), lkup_fun()) ->
+        null | undefined | avro_value().
 parse_default_value(undefined, _FieldType, _Lkup) ->
   undefined;
+parse_default_value(null, _FieldType, _Lkup) ->
+  %% skip type check on default null value definition
+  %% https://github.com/klarna/erlavro/issues/50
+  null;
 parse_default_value(Value, FieldType, Lkup) when ?IS_UNION_TYPE(FieldType) ->
   %% Strange agreement about unions: default value for an union field
   %% corresponds to the first type in this union.
