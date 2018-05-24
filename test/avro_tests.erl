e528bc1b5ff18713dddb55890425f8fb18bebc05
bka9 <keviea@gmail.com>
2018-05-24 08:57:11 -0600

    Added tests to the schema decoder and encoder to cover the default node case

--- test/avro_tests.erl
+++ test/avro_tests.erl
@@ -351,7 +351,9 @@ default_values_test() ->
                             , {<<"children">>, []}
                             ]
                           ]}
-                       ]}],
+                       ]},
+            {<<"f2">>, null},
+            {<<"f3">>, null}],
   TestFun =
     fun(Opts) ->
       RootType = "org.apache.avro.test",
