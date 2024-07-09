be31a70f78d8db79262c398425b6c7c35e5775de
zmstone <zmstone@gmail.com>
2024-07-09 09:47:19 +0200

    Fix bytes and fixed JSON value decode

--- src/avro_json_encoder.erl
+++ src/avro_json_encoder.erl
@@ -1,6 +1,6 @@
 %% coding: latin-1
 %%%-----------------------------------------------------------------------------
-%%% Copyright (c) 2013-2018 Klarna AB
+%%% Copyright (c) 2013-2024 Klarna AB
 %%%
 %%% This file is provided to you under the Apache License,
 %%% Version 2.0 (the "License"); you may not use this file
